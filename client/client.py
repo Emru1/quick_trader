fc63313a6224fca1c1536702f9c244a29d230de4
lukasz.bochniak <lukasz.bochniak@skni.umcs.pl>
2021-06-12 19:58:57 +0200

    Refaktoryzacja protokolu i klienta

--- client/client.py
+++ client/client.py
@@ -1,82 +1,77 @@
 import socket
 import json
 import time
+import sys
 
-global user, password, s
+global user, password, s, token, price
+
+def check_token(message):
+    if message["token"] != token:
+        print("Błąd uwierzytelnienia! Połączenie nie jest bezpieczne, skontaktuj się z administracją")
+        sys.exit(0)
 
 def dload():
     size = b''
     msg = b''
-    while not b'\0' in size:
+    while not b'x' in size:
         response = s.recv(1)
         size += response
     size = size[:-1]
     msg = s.recv(size)
     if (msg.size != size):
         return "error"
-    return msg.decode('utf-8')[:-4]
+    msg = msg.decode('utf-8')
+    if msg["error"]:
+        print(msg["error"])
+    return msg
 
 def dload_list():
-    temp_bool = False
-    while not temp_bool:
-        jtemp = json.loads('{"type":"ask", "data":"dload_trades", "who":user}')
-        Message = jtemp.size() + '\0' + jtemp
-        s.sendall(Message.encode('utf-8'))
-        Message = dload()
-        if Message == "error":
-            print("Something's wrong with downloading list! Trying again")
-            time.sleep(5)
-            continue
-        print(Message["data"])
+    jtemp = json.loads('{"type":"list"}')
+    message = jtemp.size() + 'x' + jtemp
+    s.sendall(message.encode('utf-8'))
+    message = dload()
+    if message == "error":
+        print("Something's wrong with downloading list! Trying again")
+        time.sleep(5)
+        dload_list()
+    else:
+        print(message["list"])
 
 def log_in():
-    while not temp_bool:
-        user = input("Login:")
-        password = input("Password:")
-        jtemp = json.loads('{"type":"login", "data":password, "who":user}')
-        Message = jtemp.size() + '\0' + jtemp
-        s.sendall(Message.encode('utf-8'))
-        Message = dload()
-        if Message == "error":
-            print("Connection lost! Try again")
-            continue
-        if Message["type"]== "login":
-            if (Message["data"]):
-                login_istrue = True
+    user = input("Login:")
+    password = input("Password:")
+    jtemp = json.loads('{"type":"auth", "username":user, "password":password}')
+    message = jtemp.size() + 'x' + jtemp
+    s.sendall(message.encode('utf-8'))
+    message = dload()
+    if message == "error" or message["error"]:
+        print("Connection lost! Try again")
+        if message["error"]:
+            print (message["error"])
+        log_in()
+    elif message["type"] == "login":
+        token = message["token"]
 
-def join_trade():
-    temp_bool = False
-    while not temp_bool:
-        trade_id = input("Select trade ID:")
-        jtemp = json.loads('{"type":"join_trade", "data":"trade_id", "who":user}')
-        Message = jtemp.size() + '\0' + jtemp
-        s.sendall(Message.encode('utf-8'))
-        Message = dload()
-        if Message == "error":
-            print("Something's wrong! Try again")
-            continue
-        temp_bool = True
+def bet():
+    jtemp = json.loads('{"type":"set_price", "price":price}')
+    Message = jtemp.size() + 'x' + jtemp
+    s.sendall(Message.encode('utf-8'))
 
 def trading():
-    jtemp = json.loads('{"type":"join_trade", "data":"trade_id", "who":user}')
-    Message = jtemp.size() + '\0' + jtemp
-    s.sendall(Message.encode('utf-8'))
-    Message = dload()
-    
+    #musisz tutaj dodać wątek nasłuchiwania który aktualizuje zmienną "price" i odbiera informacje od serwera
+    #użyj funkcji bet do wysłania nowej ceny
+
+
     #CDN
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 try:
     temp_bool = False
     s.connect(("localhost", 55555))
     log_in()
     dload_list()
-    join_trade()
     trading()
-
-
 except socket.error:
-    print("error")
+    print("Socket error")
 
 s.close()
