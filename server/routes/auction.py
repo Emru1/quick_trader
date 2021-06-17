04ca9c53f08fbae4f5033d2aa9383a16c1f98518
lukasz.bochniak <lukasz.bochniak@skni.umcs.pl>
2021-06-17 15:52:28 +0200

    Unduping code on auction.py

--- server/routes/auction.py
+++ server/routes/auction.py
@@ -1,17 +1,10 @@
-import hashlib
-import time
-
-from peewee import DoesNotExist
-
-import server.errors
 from server.database import Auction, Auth
 
 
-
-class Auction_handler:
-    '''
+class AuctionHandler:
+    """
     Klasa służąca do obsłygi aukcji po stronie serwera
-    '''
+    """
 
     def __init__(self):
         # from config import GLOBAL_CONFIG
@@ -23,26 +16,33 @@ class Auction_handler:
         self.buyer = None
         pass
 
-    def dload_auction(self):
-        self.current_auction = Auction.select().where(Auction.ended == False).order_by(Auction.start_time.asc())
+    def load_auction(self):
+        self.current_auction = Auction.select().\
+            where(not Auction.ended).\
+            order_by(Auction.start_time.asc())
         self.actual_price = self.current_auction.start_price
         self.seller = self.current_auction.seller
         self.end_time = self.current_auction.end_time
         self.item_name = self.current_auction.name
-        return None, {"Actual_price": self.actual_price, "Seller": self.seller, "End_time": self.end_time, "Item_name": self.item_name}
+        return None, {"Actual_price": self.actual_price,
+                      "Seller": self.seller,
+                      "End_time": self.end_time,
+                      "Item_name": self.item_name}
 
     def bet(self, data):
 
         bet_price = data["price"]
-        user = Auth.get(Auth.login_token == data["token"])
-        username = user.name
+        username = Auth.get(Auth.login_token == data["token"]).name
         if bet_price < self.actual_price:
             return None
         self.actual_price = bet_price
         self.buyer = username
         self.end_time += 10
-        return None, {"Actual_price": self.actual_price, "Buyer": self.buyer, "End_time": self.end_time}
+        return None, {"Actual_price": self.actual_price,
+                      "Buyer": self.buyer,
+                      "End_time": self.end_time}
 
     def end_of_time(self):
         Auction.update({Auction.ended: True, Auction.buyer: self.buyer, Auction.start_price: self.actual_price})
-        return None, {"Actual_price": self.actual_price, "Buyer": self.buyer}
+        return None, {"Actual_price": self.actual_price,
+                      "Buyer": self.buyer}
